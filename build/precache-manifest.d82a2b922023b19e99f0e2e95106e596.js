821782ee2009aef6719e2e2653fc98d255f0ab1e
Henry Hayes <hennus.hayes@gmail.com>
2020-11-23 14:29:21 +0200

    Setting up dependencies

---
+++ build/precache-manifest.d82a2b922023b19e99f0e2e95106e596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b1691d3caec4afcde8d5a9437c9009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5609d51a686de50531",
+    "url": "/static/css/2.9c82a2bb.chunk.css"
+  },
+  {
+    "revision": "d659f272dc221b88d301",
+    "url": "/static/css/main.482efe81.chunk.css"
+  },
+  {
+    "revision": "fb5609d51a686de50531",
+    "url": "/static/js/2.d506f0df.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d506f0df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d659f272dc221b88d301",
+    "url": "/static/js/main.f567c20a.chunk.js"
+  },
+  {
+    "revision": "e1fcef772f1fdaabae77",
+    "url": "/static/js/runtime-main.466e3532.js"
+  }
+]);
